44ffd7062c9b0741d4e46e759e5d894245f1ccd6
Douglas <douglasbelinato@cvccorp.com.br>
2021-06-23 14:39:34 -0300

    Fisrt commit

---
+++ module-02/src/main/java/com/lab/product/Product.java
@@ -0,0 +1,14 @@
+package com.lab.product;
+
+public class Product {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
